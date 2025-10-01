a222609e3cc1d6f76a9c179543a137e68d0edd9e
k0haku1 <138597341+k0haku1@users.noreply.github.com>
2025-10-01 14:25:25 +0300

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-DEEPSEEK_API_KEY = "io-v2-eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJvd25lciI6IjE2Yzk0OWI3LWUyY2QtNDE2Mi05Yzk3LWEzNWJiYzQ2MWRkYSIsImV4cCI6NDkxMTU0MTIyM30.NMUytr4NyUSVIYmD5XDB97e4FbqwREXuHblLyMIW6ADj5iNG-CjEzrTUWt49Ebu6WAH61Yo8fQ-RTATjAgkG4w"
-
-#"model": "meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8",
-#"model": "Intel/Qwen3-Coder-480B-A35B-Instruct-int4-mixed-ar",
-#"openai/gpt-oss-120b"
